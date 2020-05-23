8dc2f46563827ee00677783555cf65161ef45014
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-23 17:13:56 -0400

    Add comments

--- src/index.js
+++ src/index.js
@@ -2,9 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-	return <div>
-		<h1>Well hi there!</h1>
-	</div>;
+	return (
+		<div>
+		  <h1>Comment section</h1>
+			<div class="ui container comments">
+				<div class="comment">
+					<a href="#" class="avatar">
+						<img alt="avatar" />
+					</a>
+					<div class="content">
+					<a href="#" class="author">
+						Sammy
+					</a>
+					</div>
+					<div class="metadata">
+					<span class="date">
+						Today at 5:11 P.M.
+					</span>
+					</div>
+					<div class="text">
+					Nice post buddy!
+					</div>
+				</div>
+
+				<div class="comment">
+					<a href="#" class="avatar">
+						<img alt="avatar" />
+					</a>
+					<div class="content">
+					<a href="#" class="author">
+						Isaac
+					</a>
+					</div>
+					<div class="metadata">
+					<span class="date">
+						Today at 7:40 P.M.
+					</span>
+					</div>
+					<div class="text">
+					Love this a lot
+					</div>
+				</div>
+
+				<div class="comment">
+					<a href="#" class="avatar">
+						<img alt="avatar" />
+					</a>
+					<div class="content">
+					<a href="#" class="author">
+						Rudy
+					</a>
+					</div>
+					<div class="metadata">
+					<span class="date">
+						Today at 11:11 P.M.
+					</span>
+					</div>
+					<div class="text">
+					You'll be a billionaire one day
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
