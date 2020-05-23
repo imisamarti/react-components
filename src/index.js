67d37d52a30cf71d1a23774dec622daa87213992
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-23 16:58:15 -0400

    Add H1 to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+	return <div>
+		<h1>Well hi there!</h1>
+	</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
